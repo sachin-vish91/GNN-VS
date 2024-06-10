ce4080bfa1c52c9a6c12b3865c0759324af7177f
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-06-10 21:37:47 +0200

    Update prediction.sh

--- D-MPNN/prediction.sh
+++ D-MPNN/prediction.sh
@@ -5,4 +5,7 @@
 #SBATCH --partition=fast
 #SBATCH --mem=150GB
 
+# Activate conda env
+conda activate chemprop
+
 chemprop_predict --test_path HCT-116_test.csv --checkpoint_path ../model.pt --preds_path HCT-116_prediction.csv
