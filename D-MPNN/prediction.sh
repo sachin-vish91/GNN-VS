38048d37f3b19f9d8c1036d7d1cf561a55646fdb
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-06-10 21:04:37 +0200

    Update prediction.sh

--- D-MPNN/prediction.sh
+++ D-MPNN/prediction.sh
@@ -3,4 +3,4 @@
 ##t o run the prediction activate the environment and run below command.
 ## below is the simple example full description is here: https://github.com/chemprop/chemprop
 
-chemprop_predict --test_path input.csv --checkpoint_path ../fold_0/model_0/model.pt --preds_path prediction.csv
+chemprop_predict --test_path input.csv --checkpoint_path ../model.pt --preds_path prediction.csv
