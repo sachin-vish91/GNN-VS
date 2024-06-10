53ecea3c48b52aaf60a334c38674e075d38de066
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-06-10 21:10:39 +0200

    Update prediction.sh

--- D-MPNN/prediction.sh
+++ D-MPNN/prediction.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-
-##t o run the prediction activate the environment and run below command.
-## below is the simple example full description is here: https://github.com/chemprop/chemprop
+#!/bin/bash
+#SBATCH --job-name=train-HCT-116
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=50
+#SBATCH --partition=fast
+#SBATCH --mem=150GB
 
 chemprop_predict --test_path input.csv --checkpoint_path ../model.pt --preds_path prediction.csv
