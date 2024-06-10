ae50cf5aab28e7d625932c988aec0ee4a87fe693
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-06-10 21:47:04 +0200

    Update train.sh

--- D-MPNN/train.sh
+++ D-MPNN/train.sh
@@ -8,5 +8,4 @@
 # Activate conda env
 conda activate chemprop
 
-chemprop_train --data_path HCT-116_train.csv --config_path  Config.json --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --epochs 100 --save_dir ./output_dir --split_sizes 0.9 0.1 0.0
-
+chemprop_train --data_path HCT-116_train.csv --config_path  Config.json --smiles_columns SMILE --target_columns NLOGGI50 --dataset_type regression --epochs 100 --save_dir ./output_dir --split_sizes 0.9 0.1 0.0
