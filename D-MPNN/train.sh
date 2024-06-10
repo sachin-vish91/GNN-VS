ae0df0a531b017ab1820e23f3935123ae6b9187e
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-06-10 21:38:02 +0200

    Update train.sh

--- D-MPNN/train.sh
+++ D-MPNN/train.sh
@@ -5,6 +5,8 @@
 #SBATCH --partition=fast
 #SBATCH --mem=150GB
 
+# Activate conda env
+conda activate chemprop
 
 chemprop_train --data_path HCT-116_train.csv --config_path  Config.json --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --epochs 100 --save_dir ./output_dir --split_sizes 0.9 0.1 0.0
 
